18933e622276d039644b91ac364913d704dc12e9
MontreedetPame <zoozoro@hotmail.com>
2022-07-24 23:51:22 +0700

    แก้ไฟล์ migrate create_product_table

--- database/migrations/2022_07_24_164429_create_products_table.php
+++ database/migrations/2022_07_24_164429_create_products_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,12 +14,11 @@ return new class extends Migration
      */
     public function up()
     {
-          Schema::create('products', function (Blueprint $table) {
-            $table->increments('pid');
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
             $table->string('name');
             $table->decimal('price');
             $table->text('description');
-            $table->timestamps();
         });
     }
 
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        // Schema::dropIfExists('products');
+        Schema::dropIfExists('products');
     }
 };
